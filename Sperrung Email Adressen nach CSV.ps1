6718a32bea9a69b6dd9fa974483da3aa74b111bd
Tim Podszus <tim@podszus.eu>
2024-11-29 08:18:44 +0100

    Add files via upload

--- Sperrung Email Adressen nach CSV.ps1
+++ Sperrung Email Adressen nach CSV.ps1
@@ -2,24 +2,10 @@
 $Comment = Read-Host -Prompt "Comment for the Blocklist: "
 $emailAdressen = Import-Csv -Path $Path
 
-# Initialisiere eine Liste für die E-Mail-Adressen
-$emailListe = @()
 
 foreach ($email in $emailAdressen) {
-    $emailListe += $email.'Sender address'
+ $emailAdresse = $email.'Sender address'
+        New-TenantAllowBlockListItems -ListType Sender -Entries $emailAdresse -Block -NoExpiration -Notes $Comment
     
-    # Wenn die Liste 20 E-Mail-Adressen enthält, führe den Befehl aus
-    if ($emailListe.Count -eq 20) {
-        $emailAdressenGruppe = $emailListe -join ","
-        New-TenantAllowBlockListItems -ListType Sender -Entries $emailAdressenGruppe -Block -NoExpiration -Notes $Comment
-        
-        # Leere die Liste für die nächsten 20 E-Mail-Adressen
-        $emailListe.Clear()
-    }
 }
 
-# Führe den Befehl für die verbleibenden E-Mail-Adressen aus, falls weniger als 20 übrig sind
-if ($emailListe.Count -gt 0) {
-    $emailAdressenGruppe = $emailListe -join ","
-    New-TenantAllowBlockListItems -ListType Sender -Entries $emailAdressenGruppe -Block -NoExpiration -Notes $Comment
-}
